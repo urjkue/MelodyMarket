9a66c47d6f3a2278737e078093bfc3a57e84d2f9
warrennookooloo <kw.warren98@gmail.com>
2023-10-25 20:18:15 +0400

    view and controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,20 @@
 class BookingsController < ApplicationController
+  def index
+    @instrument = Instrument.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def find
+  end
 end
--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,2 +1,20 @@
 class InstrumentsController < ApplicationController
+  def index
+    @instrument = Instrument.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def find
+  end
+
+  def destroy
+  end
 end
