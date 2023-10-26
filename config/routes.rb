3ed0bff6fb874e5aed5447efb6a3a8ca84d5907f
DakshTorul <toruldaksh@gmail.com>
2023-10-26 20:31:14 +0400

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  get "/instruments", to: "instruments#index"
+  get "/catalogue", to: "instruments#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
