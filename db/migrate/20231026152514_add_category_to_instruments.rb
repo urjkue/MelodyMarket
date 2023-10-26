2bb9da494577e910f438ad507a07fd6815a5196d
warrennookooloo <kw.warren98@gmail.com>
2023-10-26 19:43:31 +0400

    seed and category

---
+++ db/migrate/20231026152514_add_category_to_instruments.rb
@@ -0,0 +1,5 @@
+class AddCategoryToInstruments < ActiveRecord::Migration[7.0]
+  def change
+    add_column :instruments, :category, :string
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# db/seeds.rb
+# Create Users
+users = []
+10.times do |i|
+  users << User.create(email: "user#{i + 1}@example.com", password: 'password')
+end
+# Create Instruments with Category
+categories = ['String', 'Wind', 'Percussion', 'Brass', 'Electronic']  # Add more categories as needed
+instruments = []
+users.each do |user|
+  10.times do
+    instruments << Instrument.create(
+      price: rand(50..300),
+      location: Faker::Address.city,
+      category: categories.sample,
+      user: user
+    )
+  end
+end
+# Create Reviews
+instruments.each do |instrument|
+  10.times do
+    Review.create(
+      comment: Faker::Lorem.sentence,
+      rating: rand(1.0..5.0).round(1),
+      instrument: instrument
+    )
+  end
+end
+# Create Bookings
+users.each do |user|
+  10.times do
+    Booking.create(
+      starting_date: Date.today + rand(1..30).days,
+      ending_date: Date.today + rand(31..60).days,
+      user: user
+    )
+  end
+end
