c1c2108b188fe036e988c447238abded5ef56172
DakshTorul <toruldaksh@gmail.com>
2023-10-27 21:52:53 +0400

    done a lot of work , just saving

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: "pages#home"
   get "/catalogue", to: "instruments#index"
   get "instrument/:id", to: "instruments#show",as:"instrument"
+  get "/brows/:category", to: "instruments#cate",as:"cateinstrument"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
