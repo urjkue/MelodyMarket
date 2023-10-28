7ed840fba3a8ac8527bc411eb24e545db389534d
DakshTorul <toruldaksh@gmail.com>
2023-10-28 15:44:59 +0400

    done

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -3,12 +3,10 @@ class InstrumentsController < ApplicationController
     @instruments = Instrument.all
   end
 
-  def show
     def show
       @ins = Instrument.find(params[:id])
       @reviews = Review.where(instrument_id: @ins.id)
     end
-  end
 
   def new
   end
