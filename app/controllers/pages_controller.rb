142c8c5a4787bc56b813e82c2f6269a59c900341
DakshTorul <toruldaksh@gmail.com>
2023-11-03 00:30:56 +0400

    some changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,13 +11,22 @@ class PagesController < ApplicationController
 
 
   def show
-
     @ins = Instrument.find(params[:id])
-   @reviews = Review.where(instrument_id: @ins.id).order(rating: :desc).limit(2)
-    @id = @ins.id
     @user_id = current_user.id
     user = User.find(@user_id)
     @email = user.email
+
+    # Check if the user has booked this instrument
+    @booking = Booking.find_by(user_id: @user_id, instrument_id: @ins.id)
+
+    if @booking.present?
+      # User has booked the instrument, fetch reviews
+      @reviews = Review.where(instrument_id: @ins.id).order(rating: :desc).limit(2)
+    else
+      # User has not booked the instrument, do something (redirect, show a message, etc.)
+      flash[:alert] = "You must book this instrument to write a review and rate it."
+      redirect_to root_path # Redirect to home or another appropriate path
+    end
   end
   def category
     @instru = Instrument.where(category: params[:category])
@@ -32,5 +41,58 @@ class PagesController < ApplicationController
     @input_value = params[:input_value]
     @instrument = Instrument.find(params[:instrument_id])
     @number_of_days = (@ending_date - @starting_date).to_i
+
+
+    @booking = Booking.new(
+      user_id: @user.id,
+      starting_date: @starting_date,
+      ending_date: @ending_date,
+      instrument_id: @instrument.id,
+    )
+    if @booking.save
+      redirect_to dashboard_path, notice: 'Booking was successfully created.'
+    else
+
+      render :booking
+    end
+
+
   end
+
+  def dashboard
+    @user_dashboard_id = current_user.id
+    @user_dashboard = User.find(@user_dashboard_id)
+    @bookings = Booking.where(user_id: @user_dashboard_id)
+    @reviews = Review.where(user_id: @user_dashboard_id)
+  end
+
+  def new_review
+    @instrument = Instrument.find(params[:id])
+    @booking = Booking.find_by(user_id: current_user.id, instrument_id: @instrument.id)
+    @review = Review.new
+  end
+
+  def create_review
+    @instrument = Instrument.find(params[:id])
+    @booking = Booking.find_by(user_id: current_user.id, instrument_id: @instrument.id)
+
+    if @booking.present?
+      @review = Review.new(review_params.merge(user_id: current_user.id, instrument_id: @instrument.id))
+
+      if @review.save
+        redirect_to instrument_path(@instrument), notice: 'Review added successfully.'
+      else
+        render :new_review, alert: 'Failed to add review. Please try again.'
+      end
+    else
+      redirect_to instrument_path(@instrument), alert: 'You must book this instrument to write a review.'
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:comment, :rating)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  get "/catalogue", to: "pages#index"
+  get "/catalogue", to: "pages#index", as:"catalogue"
   get "instrument/:id", to: "pages#show",as:"instrument"
   get "/products/:category", to: "pages#category",as:"cateinstrument"
+  get "/Abouts-us", to: "pages#Aboutus", as:"about"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "/booking", to: "pages#booking", as: "booking"
+  get "/dashboard", to: "pages#dashboard", as: "dashboard"
+  get '/instruments/:id/add-review', to: 'pages#new_review', as: 'new_review'
+  post '/instruments/:id/add-review', to: 'pages#create_review', as: 'create_review'
   # Defines the root path route ("/")
   # root "articles#index"
 end
